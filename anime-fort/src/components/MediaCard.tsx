a0f99ecb71650ddddf89adb26967ba92cbd73337
dallas kelley <alexquaesar@gmail.com>
2024-01-04 17:41:57 -0800

    MediaCards 4 days

--- anime-fort/src/components/MediaCard.tsx
+++ anime-fort/src/components/MediaCard.tsx
@@ -7,57 +7,7 @@ interface Props {
 }
 
 const MediaCard = ({ id, type, links, attributes }: Props) => {
-	return (
-		<div className="max-w-[25%]">
-			<div className="relative min-h-64">
-				<Image
-					layout="fill"
-					objectFit="cover"
-					src="https://media.kitsu.io/anime/poster_images/1/large.jpg"
-					alt="Cowboy Bebop"
-				/>
-			</div>
-			<div className="bg-[]">
-				<div>
-					<h2>Cowboy Bebop</h2>
-					<p>
-						<span>R</span> - <span>17+ violence & profanity</span>/
-						<span>1998-04-03</span>,
-					</p>
-				</div>
-				<div>
-					<div>
-						<h3>Summary</h3>
-						<p>96</p>
-					</div>
-					<p>
-						In the year 2071, humanity has colonized several of the
-						planets and moons of the solar system leaving the now
-						uninhabitable surface of planet Earth behind. The Inter
-						Solar System Police attempts to keep peace in the
-						galaxy, aided in part by outlaw bounty hunters, referred
-						to as "Cowboys". The ragtag team aboard the spaceship
-						Bebop are two such individuals. Mellow and carefree
-						Spike Spiegel is balanced by his boisterous, pragmatic
-						partner Jet Black as the pair makes a living chasing
-						bounties and collecting rewards. Thrown off course by
-						the addition of new members that they meet in their
-						travels—Ein, a genetically engineered, highly
-						intelligent Welsh Corgi; femme fatale Faye Valentine, an
-						enigmatic trickster with memory loss; and the strange
-						computer whiz kid Edward Wong—the crew embarks on
-						thrilling adventures that unravel each member's dark and
-						mysterious past little by little. Well-balanced with
-						high density action and light-hearted comedy, Cowboy
-						Bebop is a space Western classic and an homage to the
-						smooth and improvised music it is named after.
-					</p>
-				</div>
-				<div></div>
-			</div>
-			{/* render links and attributes */}
-		</div>
-	);
+	return <div className="">{/* render links and attributes */}</div>;
 };
 
 export default MediaCard;
--- anime-fort/tailwind.config.ts
+++ anime-fort/tailwind.config.ts
@@ -1,20 +1,67 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				red45: "#E50000",
+				red50: "#ff0000",
+				red55: "#ff1919",
+				red60: "#ff3333",
+				red80: "#ff9999",
+				red90: "#ffcccc",
+				red95: "#ffe5e6",
+				red99: "#fffafa",
+				black06: "#0F0F0F",
+				black08: "#141414",
+				black10: "#1A1A1A",
+				black12: "#1F1F1F",
+				black15: "#262626",
+				black20: "#333333",
+				black25: "#404040",
+				black30: "#4C4C4C",
+				grey60: "#999999",
+				grey65: "#A6A6A6",
+				grey70: "#B3B3B3",
+				grey75: "#BFBFBF",
+				grey90: "#e4e4e7",
+				grey95: "#f1f1f3",
+				grey97: "#f8f8fa",
+				grey99: "#fcfcfd",
+			},
+			backgroundColor: {
+				red45: "#E50000",
+				red50: "#ff0000",
+				red55: "#ff1919",
+				red60: "#ff3333",
+				red80: "#ff9999",
+				red90: "#ffcccc",
+				red95: "#ffe5e6",
+				red99: "#fffafa",
+				black06: "#0F0F0F",
+				black08: "#141414",
+				black10: "#1A1A1A",
+				black12: "#1F1F1F",
+				black15: "#262626",
+				black20: "#333333",
+				black25: "#404040",
+				black30: "#4C4C4C",
+				grey60: "#999999",
+				grey65: "#A6A6A6",
+				grey70: "#B3B3B3",
+				grey75: "#BFBFBF",
+				grey90: "#e4e4e7",
+				grey95: "#f1f1f3",
+				grey97: "#f8f8fa",
+				grey99: "#fcfcfd",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
